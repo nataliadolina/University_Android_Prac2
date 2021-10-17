bf0250d4ce0dd8ead4853adc2806345b8d1bf1a8
Natalia <nat.dol30@mail.ru>
2021-10-17 20:56:25 +0300

    main task has been done

--- Prac2/app/src/main/java/com/example/prac2/MainActivity.java
+++ Prac2/app/src/main/java/com/example/prac2/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.prac2;
 
+import android.annotation.SuppressLint;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener  {
+    protected TextView textView;
+    protected Button button;
+    protected int counter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.main_button);
+        textView = findViewById(R.id.main_textview);
+        button.setOnClickListener(this);
+        counter = 0;
+
+    }
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    public void onClick(View v) {
+        counter ++;
+        textView.setText("На кнопку нажали: " + counter + " раз");
     }
 }
