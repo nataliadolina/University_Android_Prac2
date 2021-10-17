32bfc9abe186f3d401533c8526b88a33381abf42
Natalia <nat.dol30@mail.ru>
2021-10-17 22:26:31 +0300

    main task + 1, 2, 3 additive tasks

--- Prac2/app/src/main/java/com/example/prac2/MainActivity.java
+++ Prac2/app/src/main/java/com/example/prac2/MainActivity.java
@@ -8,27 +8,77 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener  {
     protected TextView textView;
-    protected Button button;
+    protected Button plus_button;
+    protected Button minus_button;
+    protected Button reset_button;
     protected int counter;
+
+    private Sklonyalka sklon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        sklon = new Sklonyalka();
+        sklon.__init__("раз");
         setContentView(R.layout.activity_main);
 
-        button = findViewById(R.id.main_button);
-        textView = findViewById(R.id.main_textview);
-        button.setOnClickListener(this);
-        counter = 0;
+        plus_button = findViewById(R.id.plus_button);
+        minus_button = findViewById(R.id.minus_button);
+        reset_button = findViewById(R.id.reset_button);
 
+
+        textView = findViewById(R.id.main_textview);
+        plus_button.setOnClickListener(this);
+        minus_button.setOnClickListener(this);
+        reset_button.setOnClickListener(this);
+        counter = 0;
     }
 
     @SuppressLint("SetTextI18n")
     @Override
     public void onClick(View v) {
-        counter ++;
-        textView.setText("На кнопку нажали: " + counter + " раз");
+        if (v.getId() == R.id.plus_button){
+            counter ++;
+        } else if (v.getId() == R.id.minus_button){
+            counter --;
+        } else {
+            counter = 0;
+        }
+
+        textView.setText("На кнопку нажали: " + counter + " " + sklon.ModifyEnding(Math.abs(counter)));
     }
+}
+
+class Sklonyalka {
+    protected String _word;
+    protected Map<Integer,String> dictionary;
+    public void __init__(String word) {
+        _word = word;
+
+        dictionary = new HashMap<Integer,String>();
+        dictionary.put(0, "");
+        dictionary.put(1, "");
+        dictionary.put(2, "а");
+        dictionary.put(3, "а");
+        dictionary.put(4, "а");
+        dictionary.put(5, "");
+        dictionary.put(6, "");
+        dictionary.put(7, "");
+        dictionary.put(8, "");
+        dictionary.put(9, "");
+    }
+
+    public String ModifyEnding(int num){
+        if (num / 10 == 1) {
+            return _word;
+        }
+        return _word + dictionary.get(num%10);
+    }
+
 }
